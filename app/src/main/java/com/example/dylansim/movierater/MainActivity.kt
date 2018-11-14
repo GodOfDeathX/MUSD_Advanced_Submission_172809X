5013f990d8816e8ac0e04a3d17877f41a55850d7
GodOfDeathX <172809X@mymail.nyp.edu.sg>
2018-11-14 21:46:54 +0800

    Adding Checkboxes

--- app/src/main/java/com/example/dylansim/movierater/MainActivity.kt
+++ app/src/main/java/com/example/dylansim/movierater/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.dylansim.movierater
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkboxNotSuitable.setOnClickListener(View.OnClickListener {
+            if (checkboxNotSuitable.isChecked == true){
+                checkboxLayout.setVisibility(View.VISIBLE);
+            }
+        })
     }
 }
