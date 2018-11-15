6eef953911a25f9ccc7621f24ae47f19c0cc14be
GodOfDeathX <172809X@mymail.nyp.edu.sg>
2018-11-15 13:04:46 +0800

    Adding Checkboxes

--- app/src/main/java/com/example/dylansim/movierater/MainActivity.kt
+++ app/src/main/java/com/example/dylansim/movierater/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dylansim.movierater
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +14,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         checkboxNotSuitable.setOnClickListener(View.OnClickListener {
-            if (checkboxNotSuitable.isChecked == true){
-                checkboxLayout.setVisibility(View.VISIBLE);
+            if (checkboxNotSuitable.isChecked == true) {
+                checkboxLayout.visibility = View.VISIBLE
+            }
+            if (checkboxNotSuitable.isChecked == false) {
+                checkboxLayout.visibility = View.GONE
             }
         })
+        addMovie.setOnClickListener {
+            var counter = 0
+            val lang = findViewById<RadioButton>(languageRadioGroup.checkedRadioButtonId).text
+            val suitable = checkboxNotSuitable.isChecked
+            var violenceCheck = ""
+            var languageCheck = ""
+            if (violence.isChecked || languageUsed.isChecked) {
+                violenceCheck = "${violence.text}"
+                languageCheck = "${languageUsed.text}"
+            } else if (languageUsed.isChecked) {
+                languageCheck = "${languageUsed.text}"
+            } else if (violence.isChecked) {
+                violenceCheck = "${violence.text}"
+            }
+            if (movieName.text.isEmpty()) {
+                movieName.error = "Field Empty"
+                counter++
+            }
+            if (movieDescription.text.isEmpty()) {
+                movieDescription.error = "Field Empty"
+                counter++
+            }
+            if (movieReleaseDate.text.isEmpty()) {
+                movieReleaseDate.error = "Field Empty"
+                counter++
+            }
+            if (counter == 0) Toast.makeText(
+                this,
+                "title = ${movieName.text} \n Overview = ${movieDescription.text} \n Release Date ${movieReleaseDate.text} \n Language = $lang \n Suitable for all ages = $suitable \n Reason: \n $violenceCheck \n $languageCheck",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
+
